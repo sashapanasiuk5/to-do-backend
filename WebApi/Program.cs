16d2ebb5a38d7d68ba998c48bca4126eb308bb84
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-07-22 01:14:29 +0300

    configure mediatr

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,8 @@
+using System.Reflection;
+using Core.Actions.Task.Create;
+using WebApi.Controllers;
+using Task = DataAccess.Entities.Task;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var assemblies = new[]
+{
+    Assembly.GetAssembly(typeof(TaskController)),
+    Assembly.GetAssembly(typeof(CreateTaskCommand)),
+};
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies!));
 
 var app = builder.Build();
 
