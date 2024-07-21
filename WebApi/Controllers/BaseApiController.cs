b851ef76da26d6f52030c137f35f2938d67eca41
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-07-22 01:55:32 +0300

    add endpoint for getting tasks

---
+++ WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,23 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class BaseApiController: ControllerBase
+{
+    protected ActionResult HandleResult<T>(Result<T> result)
+    {
+        if (result.IsSuccess)
+        {
+            if (result.Value is Unit)
+            {
+                return Ok(null);
+            }
+
+            return Ok(result.Value);
+        }
+
+        return BadRequest(result.Reasons);
+    }
+}
--- WebApi/Controllers/TaskController.cs
+++ WebApi/Controllers/TaskController.cs
@@ -1,10 +1,22 @@
+using Core.Actions.Task.GetAll;
+using FluentResults;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
 
 [ApiController]
 [Route("tasks")]
-public class TaskController : ControllerBase
+public class TaskController : BaseApiController
 {
-
+    private IMediator _mediator;
+    public TaskController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllTasksAsync()
+    {
+        return HandleResult(await _mediator.Send(new GetAllTasksQuery()));
+    }
 }
