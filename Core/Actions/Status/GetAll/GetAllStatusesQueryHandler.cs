41c94ad161fa23a8e9391875445f406bef33043e
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-07-22 23:28:06 +0300

    add logic for retrieving statuses

--- Core/Actions/Status/GetAll/GetAllStatusesQueryHandler.cs
+++ Core/Actions/Status/GetAll/GetAllStatusesQueryHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using DataAccess.Repositories.Interfaces;
 using FluentResults;
 using MediatR;
 
@@ -9,8 +10,14 @@ namespace Core.Actions.Status.GetAll;
 
 public class GetAllStatusesQueryHandler: IRequestHandler<GetAllStatusesQuery, Result<List<DataAccess.Entities.Status>>>
 {
-    public Task<Result<List<DataAccess.Entities.Status>>> Handle(GetAllStatusesQuery request, CancellationToken cancellationToken)
+    private readonly IStatusRepository _statusRepository;
+
+    public GetAllStatusesQueryHandler(IStatusRepository statusRepository)
     {
-        throw new NotImplementedException();
+        _statusRepository = statusRepository;
+    }
+    public async Task<Result<List<DataAccess.Entities.Status>>> Handle(GetAllStatusesQuery request, CancellationToken cancellationToken)
+    {
+        return Result.Ok(_statusRepository.GetAll());
     }
 }
