ce738109d1af878e0c18bd974f298a030991d87f
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-07-22 22:18:26 +0300

    add deleting logic

--- Core/Actions/Task/Delete/DeleteTaskCommandHandler.cs
+++ Core/Actions/Task/Delete/DeleteTaskCommandHandler.cs
@@ -1,12 +1,20 @@
-﻿using FluentResults;
+﻿using DataAccess.Repositories.Interfaces;
+using FluentResults;
 using MediatR;
 
 namespace Core.Actions.Task.Delete;
 using DataAccess.Entities;
 public class DeleteTaskCommandHandler: IRequestHandler<DeleteTaskCommand, Result<Unit>>
 {
-    public Task<Result<Unit>> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
+    private readonly ITaskRepository _taskRepository;
+
+    public DeleteTaskCommandHandler(ITaskRepository taskRepository)
     {
-        
+        _taskRepository = taskRepository;
+    }
+    public async Task<Result<Unit>> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
+    {
+        _taskRepository.Delete(request.taskId);
+        return Result.Ok(new Unit());
     }
 }
