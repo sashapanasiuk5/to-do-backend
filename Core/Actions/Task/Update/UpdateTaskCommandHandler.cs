0b9966401c3d7af0bc6331c385e14b962dba8d20
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-07-22 21:12:47 +0300

    add updating functionality

--- Core/Actions/Task/Update/UpdateTaskCommandHandler.cs
+++ Core/Actions/Task/Update/UpdateTaskCommandHandler.cs
@@ -2,14 +2,34 @@
 using FluentResults;
 using System.Threading;
 using System.Threading.Tasks;
+using Core.Mapping;
+using DataAccess.Repositories.Interfaces;
 using MediatR;
 
 namespace Core.Actions.Task.Update;
-
+using DataAccess.Entities;
 public class UpdateTaskCommandHandler: IRequestHandler<UpdateTaskCommand, Result<Unit>>
 {
-    public Task<Result<Unit>> Handle(UpdateTaskCommand request, CancellationToken cancellationToken)
+    private readonly IStatusRepository _statusRepository;
+    private readonly ITaskRepository _taskRepository;
+
+    public UpdateTaskCommandHandler(IStatusRepository statusRepository, ITaskRepository taskRepository)
     {
-        throw new NotImplementedException();
+        _statusRepository = statusRepository;
+        _taskRepository = taskRepository;
+    }
+    public async Task<Result<Unit>> Handle(UpdateTaskCommand request, CancellationToken cancellationToken)
+    {
+        Task task = request.dto.ToTask();
+        task.Id = request.id;
+        Status? status = _statusRepository.GetById(request.dto.StatusId);
+        if (status != null)
+        {
+            task.Status = status;
+            _taskRepository.Update(task);
+            _taskRepository.SaveChanges();
+            return new Unit();
+        }
+        return Result.Fail("Status is not found"); 
     }
 }
